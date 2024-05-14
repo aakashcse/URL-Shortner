faebfbc7cb876ed2c85d6f36eac22a3cf713ace8
aakashcse <aakashvtu@gmail.com>
2024-05-14 23:22:32 +0530

    URL-Shortner-EXpress-mongo-ejs

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
+const shortId = require("shortid");
+const app = express();
+
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      "mongodb+srv://richie:richie@cluster0.rphiz.mongodb.net/urlShortner"
+    );
+    console.log("MongoDB database connected");
+  } catch (err) {
+    console.log("MongoDB connection failed ", err);
+  }
+};
+
+
+
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  console.log(__dirname);
+  const shortUrls = await ShortUrl.find();
+  res.render("index", { shortUrls: shortUrls });
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await ShortUrl.create({ full: req.body.fullUrl });
+  res.redirect("/");
+});
+
+app.get("/:shortUrl", async (req, res) => {
+  const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl });
+  if (shortUrl == null) return res.sendStatus(404);
+
+  shortUrl.clicks++;
+  shortUrl.save();
+
+  res.redirect(shortUrl.full);
+});
+
+app.listen(5000, () => {
+  connect();
+  console.log("server running on port 5000");
+});
